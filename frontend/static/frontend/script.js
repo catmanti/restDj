bfc49920d0d0ca93f1cdb235fb34a7794f482ce4
catmanti <ssomaratne@gmail.com>
2021-03-23 21:18:55 +0530

    Completed

--- frontend/static/frontend/script.js
+++ frontend/static/frontend/script.js
@@ -29,20 +29,25 @@ function buildList() {
     })
     .then((data) => {
       let wrapper = document.getElementById('list-wrapper');
-      wrapper.innerHTML = `${data.map((lst) => `
+      wrapper.innerHTML = `${data.map((lst) => {
+        let titleDiv = `<div  style="flex:10" id="t${lst.id}" class='title'>${lst.title}</div>`
+        if (lst.completed) {
+          titleDiv = `<strike  style="flex:10" id="t${lst.id}" class='title'>${lst.title}</strike>`
+        }
+        return `
     <div id="data-row-${lst.id}" class="flex-wrapper task-wrapper">
-      <div  style="flex:10" id="t${lst.id}">
-        ${lst.title}
-      </div>
+      ${titleDiv}
       <div  style="flex:.1">
-        <button type="button" id="${lst.id}" class="btn btn-info edit"><i class="fas fa-list-alt"></i></button>
+        <button type="button" id="${lst.id}" class="btn btn-info edit">
+        <i class="fas fa-list-alt"></i></button>
       </div>
       <div  style="flex:.5">
-        <button type="button" id="${lst.id}" class="btn btn-outline-dark delete"><i class="fas fa-trash-alt"></i></button>
+        <button type="button" id="${lst.id}" class="btn btn-outline-dark delete">
+        <i class="fas fa-trash-alt"></i></button>
       </div>
     </div>
-
-    `).join('')} `
+    `;
+      }).join('')} `
       //  Detect the Edit button click event and assign that title to form
       var btns = document.querySelectorAll('.edit');
       for (i of btns) {
@@ -58,7 +63,20 @@ function buildList() {
         i.addEventListener('click', function () {
           deleteItem(this.id);
         });
+      }
+      let title = document.querySelectorAll('.title');
+      for (i of title) {
+        i.addEventListener('click', function () {
+          let isStriked;
 
+          if (this.tagName == 'DIV') {
+            isStriked = true;
+          } else {
+            isStriked = false;
+
+          }
+          strikeUnstrike(this.id.substring(1), isStriked, this.innerText)
+        })
       }
       // console.log(data);
     })
@@ -101,13 +119,25 @@ form.addEventListener('submit', function (e) {
 })
 
 function deleteItem(id) {
-  console.log('Delete clicked')
   fetch(`http://127.0.0.1:8000/api/task-delete/${id}/`, {
     method: 'DELETE',
     headers: {
       'Content-type': 'application/json',
       'X-CSRFToken': csrftoken,
     }
+  }).then((response) => {
+    buildList()
+  })
+}
+
+function strikeUnstrike(id, isCompleted, thistitle) {
+  fetch(`http://127.0.0.1:8000/api/task-update/${id}/`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'X-CSRFToken': csrftoken,
+    },
+    body: JSON.stringify({ 'title': thistitle, 'completed': isCompleted })
   }).then((response) => {
     buildList()
   })
